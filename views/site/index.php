e70801eeb69490e221d962f5aba0fab0a14a0dc7
Alexander Mishin <alexander.mishin.2001@bk.ru>
2021-02-17 21:38:55 +0300

    Старая вёрстка

--- views/site/index.php
+++ views/site/index.php
@@ -11,30 +11,43 @@ $this->registerJsFile('/web/js/main.js');
 <div class="row">
 <!-- Главный блок с описанием -->
 <div class="home-block">
-	<div class="desk-block">
+	
+		<div class="desk-block">
 		<h1 class="Name" style="color: white; font-size: 64px; letter-spacing: 5px; font-family: 'masson';">THE <span class="redWord" style="color: #B33939;">WITCHER</span></h1>
 		<p class="desk-seria" style="color: white; font-size: 18px;">Геральт из Ривии мутировавший охотник на монстров по<br> найму, отправляется навстречу своей судьбе в неспокойном<br> мире, где люди часто оказываются более злыми, чем звери.</p><br>
 		<p class="desk-cast" style="color: white; font-size: 16px;">В ролях: Генри Кавилл, Аня Чалотра, Фрейа Аллан</p>
 		<p class="desk-cast" style="color: white; font-size: 16px;">Создатели: Лорен Шмидт Хисрич</p>
-		<button type="button" class="btn btn-danger" id="scroll_bottom" onclick="MyScroll('#kek')">СМОТРЕТЬ ОНЛАЙН</button>
+		<button type="button" class="btn btn-danger" onclick="MyScroll()">СМОТРЕТЬ ОНЛАЙН</button>
+	
 	</div>
+	
 </div>
-<!-- конец главного блока -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script>
-function MyScroll (id){
-	var offset = 0;
-	$('html, body').animate({
-	scrollTop: $(id).offset ().top - offset
-	}, 500);
-	return false;
-// console.log("qwerty"); 
-
-}
-</script>
-
-<!-- Блок с сериалом -->
-<div class="seria-block" id="kek">
+<!-- end main block -->
+<!-- news block -->
+<div class="news-block">
+	<div class="new-cart">
+		<div  class="image-news"><img src="/web/uploads/Posada.jpg"></div>
+		<div  class="title-news"><h3>Еще больше крупных VFX-студий присоединяются к The Witcher Season 2</h3></div>
+		
+		<div  class="text-news"><p>Йеннифэр, терпящая издевательства и пренебрежение, неожиданно находит способ сбежать. Охота Геральта на «дьявола» идет не по плану. Цири ищет убежище среди людей</p></div>
+	</div>
+	<div class="new-cart">
+		<div  class="image-news"><img src="/web/uploads/cintra.jpg"></div>
+		<div  class="title-news"><h3>Ведьмак снимет воспоминания о Цинтре во втором сезоне</h3></div>
+		
+		<div  class="text-news"><p>Йеннифэр, терпящая издевательства и пренебрежение, неожиданно находит способ сбежать. Охота Геральта на «дьявола» идет не по плану. Цири ищет убежище среди людей</p></div>
+	</div>
+	<div class="new-cart">
+		<div  class="image-news"><img src="/web/uploads/eskel.jpg"></div>
+		<div  class="title-news"><h3>Спойлер: Netflix внесет большие изменения в роль Эскеля в Ведьмаке </h3></div>
+		
+		<div  class="text-news"><p>Йеннифэр, терпящая издевательства и пренебрежение, неожиданно находит способ сбежать. Охота Геральта на «дьявола» идет не по плану. Цири ищет убежище среди людей</p></div>
+	</div>
+	
+</div>
+<!-- end news block -->
+<!-- video block -->
+<div class="seria-block" id="el">
 	<p style="font-size: 24px; color: white;">Эпизоды | <span style="color: #B33939;">Ведьмак</span></p>
 	<p style="font-size: 18px; color: white;">1 сезон</p>
 	<!-- блок серии -->
@@ -56,11 +69,16 @@ foreach ($seria as  $model) {
 <div>
 	
 </div>
-<!-- Конец блока с сериалом -->
+<!-- end video bloc -->
 </div>
-
-
-
-
+<!-- scroll script -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>
+function MyScroll(){
+	const el = document.getElementById('el');
+	el.scrollIntoView({behavior: "smooth"});
+}
+</script>
+<!-- end script -->
 
 
--- views/site/watch.php
+++ views/site/watch.php
@@ -3,18 +3,5 @@ $this->registerCssFile("/web/css/watch.css");
 
 ?>
 <div class="row">
-	<!-- main block -->
-	<div class="header-block">
-		<div class="watch-block">
-			<div class="title-block"><h2 style=" color: white; ">2.Начало Конца</h2></div>
-			<div class="description">
-			<div class="display-bloc"><iframe src="https://drive.google.com/file/d/125kf9S2Bi9z15pdcVTyIoKNu3l7k59Ah/preview" allowfullscreen="sandbox"  width="100%" height="100%"></iframe></div>
-			<h4 style="color: white;">Враждебные горожане и хитрая ведьма встречают Геральта в Блавикене. Цири выясняет, что ее королевство рушится, потому что Нильфгаард решает завоевать Цинтру.</h4></div>
-			<div class="button-bloc">
-				<div class="btn"><button class="prev-btn"><h4 class="prev">1 серия</h4></button></div>
-				<div class="btn"><button class="episod-btn"><h4>Список всех серий</h4></button></div>
-				<div class="btn"><button class="next-btn"><h4 class="next">3 серия</h4></button></div>
-			</div>
-		</div>
-	</div>
+
 </div>
