32079736d81d762b63f47396dc3f0f5252230ca4
Alexander Mishin <alexander.mishin.2001@bk.ru>
2021-03-12 23:17:12 +0300

    slider

--- views/site/index.php
+++ views/site/index.php
@@ -9,6 +9,7 @@ $this->registerCssFile("/web/css/main.css");
 $this->registerJsFile('/web/js/main.js');
 ?>
 
+
 <!-- Первая секция -->
 <section class="header">
     <div class="container-fluid">
@@ -29,21 +30,55 @@ $this->registerJsFile('/web/js/main.js');
     </div>
 </section>
 <!-- Конец первой секции -->
+<div class="container-fluid">
+    <div class="row">
+    <section class="wrapper">
+    <h1>Промо ролики:</h1>
+    <div class="slider-container">
+        <div class="slider-track">
+            <div class="slider-item"></div>
+            <video class="slider-item" src="/web/uploads/books.mp4" controls
+></video>
+            <video class="slider-item" src="/web/uploads/luck.mp4" controls
+></video>
+            <video class="slider-item" src="/web/uploads/walk.mp4" controls
+></video>
+            <video class="slider-item" src="/web/uploads/vid.mp4" controls
+></video>
+            <video class="slider-item" src="/web/uploads/vid.mp4" controls
+></video>
+            <!-- <div class="slider-item"></div>
+            <div class="slider-item"></div>
+            <div class="slider-item"></div>
+            <div class="slider-item"></div>
+            <div class="slider-item"></div>
+            <div class="slider-item"></div>
+            <div class="slider-item"></div>
+            <div class="slider-item"></div> -->
+        </div>
+    </div>
+    <div class="slider-buttons">
+        <button class="btn-prev">Prev</button>
+        <button class="btn-next">Next</button>
+    </div>
+    
+</section>
+    </div>
+</div>
 
 <!-- Секция контента -->
 
 <section class="content" id="content">
-    <div class="container-fluid">
-        
+    <div class="container-fluid">        
 
-        <p id="scroll" style="font-size: 24px; margin-top: 10px;">Эпизоды | <span style="color: #B33939;">Ведьмак</span></p>
-        <p style="font-size: 18px; margin-top: 10px;">Сезон | <span>1</span></p>
+        <h1 id="scroll">Эпизоды Ведьмак</h1>
+        <p  style="font-size: 18px; margin-top: 10px;">1 | Сезон</p>
         <div class="row">
 
             <?php
             foreach ($seria as  $model) {
                 echo'
-                <div class="col-3">
+                <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-3 ">
                  <div class="card">
                  <a  href="'.Url::toRoute(['site/watch', 'id'=> $model->id]).'">'.Html::img('@web/uploads/preview/'.$model->preview, ['alt' => 'My logo']).'</a>
                             <h3>'.$model->name.'</h3>
@@ -57,6 +92,62 @@ $this->registerJsFile('/web/js/main.js');
         </div>
     </div>
 </section>
-    
+<script>
+    $(document).ready(function(){
+        let position = 0;
+        const slidesToShow = 3;
+        const slidesToScroll = 2;
+        const container = $('.slider-container');
+        const track = $('.slider-track');
+        const item = $('.slider-item');
+        const btnPrev = $('.btn-prev');
+        const btnNext = $('.btn-next');
+        const itemsCount = item.length;
+        const itemWidth = container.width() / slidesToShow;
+        const movePosition = slidesToScroll * itemWidth;
+
+        item.each(function(index, item){
+            $(item).css({
+                minWidth: itemWidth,
+            });
+        });
+
+
+        btnNext.click(function(){
+            const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+            position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+            setPosition();
+            checkBtns();
+            
+        });
+
+        btnPrev.click(function(){
+            const itemsLeft = Math.abs(position) / itemWidth;
+            position += itemsLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+
+         
+            setPosition();
+            checkBtns();
+            
+        });
+        
+        const setPosition = () => {
+            track.css({
+                transform: `translateX(${position}px)`
+            });
+        };
+         const checkBtns = () => {
+            btnNext.prop(
+                'disabled',
+                position <= - (itemsCount - slidesToShow) * itemWidth
+
+            );
+            btnPrev.prop('disabled', position === 0);
+        };
+
+        checkBtns();
+    });   
+</script> 
+
 
 <!-- конец секции контента -->
